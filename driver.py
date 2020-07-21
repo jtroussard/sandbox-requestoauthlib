d0df09e169c78235ff0b34437f1b31a0a76a0aa4
e6737e6 <jacques.troussard@mckesson.com>
2020-07-21 00:20:50 -0400

    init commit, ssl error

---
+++ driver.py
@@ -0,0 +1,56 @@
+import configparser
+import os
+import sys
+
+import requests
+from requests_oauthlib import OAuth2Session
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+try:
+    platform = str(sys.argv[1])
+except IndexError:
+    sys.exit(0)
+
+config = configparser.ConfigParser()
+config.read("credentials.ini")
+
+res_client_id = config[platform]['client_id']
+res_client_secret = config[platform]['client_secret']
+res_scope = config[platform]['scope']
+res_redirect_url = config[platform]['redirect_url']
+res_auth_url = config[platform]['auth_url']
+res_token_url = config[platform]['token_url']
+res_api_call = config[platform]['api_call']
+
+print(res_client_id)
+
+token = {}
+
+# Start the example code
+# Set environment variables
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+target_platform = OAuth2Session(res_client_id, redirect_uri=res_redirect_url, scope=res_scope, token=token)
+
+# Redirect user to LinkedIn for authorization
+authorization_url, state = target_platform.authorization_url(res_auth_url)
+print(f"Please go here and authorize: {authorization_url}")
+
+print(requests.get("http://python.org"))
+
+# # Get the authorization verifier code from the callback url
+# redirect_response = input('Paste the full redirect URL here:')
+#
+# # Fetch the access token
+# target_platform.fetch_token(res_token_url, client_secret=res_client_secret,
+#                             include_client_id=True,
+#                             authorization_response=redirect_response)
+#
+# print("Fetched token")
+#
+# # Fetch a protected resource, i.e. user profile
+# r = target_platform.get('https://api.linkedin.com/v2/me')
+# print(r.content)
